764ff0a44fa1a222c6f74acfb5cb8e617699aad5
David Ugwu <97189065+Cu19aaa@users.noreply.github.com>
2024-06-02 16:48:43 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,9 +32,9 @@ const Home = () => {
         <div className='flex flex-col justify-center'>
           <p className='text-black-600'>Hi, My name is </p>
           <h1 className='text-4xl sm:text-5xl font-bold text-[#B0B0B0]'>Daniel Igbokwe</h1>
-          <h2 className='text-4xl sm:text-5xl font-bold text-[#B0B0B0]'>I am a </h2>
+          <h2 className='text-4xl sm:text-5xl font-bold text-[#B0B0B0]'>I am a/an </h2>
           <ReactTyped className='text-4xl sm:text-5xl font-bold text-[#1C1C1C]'
-            strings={['Machine Learning', 'Software Engineer']} typeSpeed={120} backSpeed={140} loop />
+            strings={['AI Engineer', 'Software Engineer']} typeSpeed={120} backSpeed={140} loop />
 
           <p className='text-[#B0B0B0] py-4 max-w-[700px]'>I specialize in developing tailored AI solutions, leveraging TensorFlow and Azure AI to optimize user experiences and operational efficiencies. I am focused on building end-to-end machine learning applications.
 
